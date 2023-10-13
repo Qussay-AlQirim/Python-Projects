e1b0812f74bd6fc19cf28d0ef7056b612914d264
Qussay Al-Qirim <113514082+Qussay1242@users.noreply.github.com>
2023-10-13 17:57:58 +1300

    Add files via upload

--- Practice_Projects/indoor/indoor.py
+++ Practice_Projects/indoor/indoor.py
@@ -1,3 +1,6 @@
+'''implement a program in Python that prompts the user for input and then outputs that same input in lowercase. Punctuation and whitespace should be outputted unchanged. 
+You’re welcome, but not required, to prompt the user explicitly, as by passing a str of your own as an argument to input.'''
+
 name = str(input('Please type something you would like to say:'))
 
 print(name.strip().lower())
