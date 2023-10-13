40604fcaa8268b5980520fbaa8c1065b3567845c
Qussay Al-Qirim <113514082+Qussay1242@users.noreply.github.com>
2023-10-13 17:58:14 +1300

    Add files via upload

--- Practice_Projects/playback/playback.py
+++ Practice_Projects/playback/playback.py
@@ -1,3 +1,6 @@
+'''implement a program in Python that prompts the user for input and then outputs that same input, 
+replacing each space with ... (i.e., three periods).'''
+
 word = input('Please type a sentence:')
 
 print(word.strip().replace(' ','...'))
